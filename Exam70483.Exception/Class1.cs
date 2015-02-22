5c3126282847af79fae3890dfffab449eebc9ca4
Aurelien Souchet <souchet.aurelien@gmail.com>
2015-02-22 04:34:10 +0100

    adding projects

---
+++ Exam70483.Exception/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam70483.Exception
+{
+    public class Class1
+    {
+    }
+}
